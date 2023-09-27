4abb9eedc39fd247618805735182cb71a7ebf076
Ahmad Shaja AZIMI <ahmad.shojaazimi@gmail.com>
2023-09-27 15:37:00 +0300

    create vehicles

---
+++ src/components/Vehicles.js
@@ -0,0 +1,52 @@
+import car1 from "../assets/car1_1.webp";
+import car2 from "../assets/car1_2.avif";
+import car3 from "../assets/car1_3.png";
+import car4 from "../assets/car1_4.avif";
+const vehicles = [
+  {
+    name: "Mercedes-Benz E-Class",
+    img: car4,
+    price: 45,
+    model: "Mercedes",
+    year: "2023",
+    Doors: 4,
+    AC: "Yes",
+    Transmission: "Automatic",
+    Fuel: "Gasoline",
+  },
+  {
+    name: "Honda Accord",
+    img: car3,
+    price: 35,
+    model: "Honda Accord",
+    year: "2023",
+    Doors: 3,
+    AC: "Yes",
+    Transmission: "Manual",
+    Fuel: "Petrol",
+  },
+  {
+    name: "Nissan Altima",
+    img: car2,
+    price: 32,
+    model: "Nissan Altima",
+    year: "2023",
+    Doors: 4,
+    AC: "Yes",
+    Transmission: "Automatic",
+    Fuel: "Electronic",
+  },
+  {
+    name: "Volkswagen Passat",
+    img: car1,
+    price: 37,
+    model: "Volkswagen Passat",
+    year: "2023",
+    Doors: 4,
+    AC: "Yes",
+    Transmission: "Automatic",
+    Fuel: "Gasoline",
+  },
+];
+
+export default vehicles;
